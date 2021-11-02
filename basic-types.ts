6f337c89fdc896455cb01bd87e9a3b1a74b46736
changxianglin <281532612@qq.com>
2021-11-03 00:36:29 +0800

    add basic

---
+++ basic-types.ts
@@ -0,0 +1,18 @@
+const isDone: boolean = false
+
+let age: number = 18
+
+let firstName: string = 'chang'
+let message: string = `Hello, ${firstName}`
+
+let u: undefined = undefined
+let n: null = null
+
+let num: number = undefined
+
+let notSure: any = 2
+notSure = 'is string?'
+notSure = true
+
+notSure.myName
+notSure.getName()
